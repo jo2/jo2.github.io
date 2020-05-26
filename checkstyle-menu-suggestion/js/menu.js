b4959d54fbf33b4bb8e105129cf43de965047061
Johannes Teklote <johannes.teklote@web.de>
2020-05-26 13:03:53 +0200

    update suggestions

--- checkstyle-menu-suggestion/js/menu.js
+++ checkstyle-menu-suggestion/js/menu.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', () => {
-    // TODO save information on visible and hidden menu in session storage
     const navButton = document.querySelector('a[title="Menu"]');
     const navColumn = document.querySelector('#leftColumn');
 
@@ -7,26 +6,51 @@ window.addEventListener('load', () => {
         e.preventDefault();
         if (navColumn.classList.contains('hidden')) {
             navColumn.classList.remove('hidden');
-            sessionStorage.setItem('menuHidden', 'false');
+            let currentElement = document.querySelector('#navcolumn strong');
+            console.log(currentElement);
+            if (currentElement) {
+                currentElement = currentElement.parentElement.parentElement;
+                while (currentElement.classList.contains('hidden') && currentElement.tagName === 'UL') {
+                    currentElement.classList.remove('hidden');
+                    currentElement = currentElement.parentElement.parentElement;
+                }
+            }
         } else {
             navColumn.classList.add('hidden');
-            sessionStorage.setItem('menuHidden', 'true');
         }
     });
 
+    document.querySelectorAll('#navcolumn ul').forEach(element => {
+        element.classList.add('hidden');
+        element.previousElementSibling.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log(element.classList);
+            document.querySelectorAll('#navcolumn ul').forEach(otherElement => {
+                if (otherElement !== element) {
+                    otherElement.classList.add('hidden');
+                }
+            });
+
+            let parent = element.parentElement.parentElement;
+            while (parent.classList.contains('hidden') && parent.tagName === 'UL') {
+                parent.classList.remove('hidden');
+                parent = parent.parentElement.parentElement;
+            }
+
+            if (element.classList.contains('hidden')) {
+                element.classList.remove('hidden');
+            } else {
+                element.classList.add('hidden');
+            }
+        });
+    });
+
     if (window.innerWidth <= 600) {
-        if (sessionStorage.getItem('menuHidden') === 'true') {
-            navColumn.classList.add('hidden');
-        } else {
-            navColumn.classList.remove('hidden');
-        }
-        // if (navButton.classList.contains('hidden')) {
+        navColumn.classList.add('hidden');
         navButton.classList.remove('hidden');
-        console.log('display menu toggle');
-        // }
     } else {
         navButton.classList.add('hidden');
         navColumn.classList.remove('hidden');
-        console.log('hide menu toggle');
     }
+    console.log(navColumn.classList)
 });
