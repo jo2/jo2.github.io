8ae101d82412b79c62845882ab791852f7109afa
Johannes Teklote <johannes.teklote@web.de>
2020-06-01 16:21:10 +0200

    add menu bar, move it to the right

--- checkstyle-menu-suggestion/js/menu.js
+++ checkstyle-menu-suggestion/js/menu.js
@@ -1,48 +1,51 @@
-window.addEventListener('load', () => {
+const renderCollapsibleMenu = () => {
     const navButton = document.querySelector('a[title="Menu"]');
     const navColumn = document.querySelector('#leftColumn');
+    const navBar = document.querySelector('#breadcrumbs .xright');
+
+    navButton.innerHTML = '<div class="burger-bar"></div><div class="burger-bar"></div><div class="burger-bar"></div>';
+
+    // document.querySelectorAll('#navcolumn ul').forEach(element => {
+    //     element.classList.add('hidden');
+    //     element.previousElementSibling.addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         console.log(element.classList);
+    //         document.querySelectorAll('#navcolumn ul').forEach(otherElement => {
+    //             if (otherElement !== element) {
+    //                 otherElement.classList.add('hidden');
+    //             }
+    //         });
+    //
+    //         let parent = element.parentElement.parentElement;
+    //         while (parent.classList.contains('hidden') && parent.tagName === 'UL') {
+    //             parent.classList.remove('hidden');
+    //             parent = parent.parentElement.parentElement;
+    //         }
+    //
+    //         if (element.classList.contains('hidden')) {
+    //             element.classList.remove('hidden');
+    //         } else {
+    //             element.classList.add('hidden');
+    //         }
+    //     });
+    // });
 
     navButton.addEventListener('click', (e) => {
         e.preventDefault();
         if (navColumn.classList.contains('hidden')) {
             navColumn.classList.remove('hidden');
-            let currentElement = document.querySelector('#navcolumn strong');
-            console.log(currentElement);
-            if (currentElement) {
-                currentElement = currentElement.parentElement.parentElement;
-                while (currentElement.classList.contains('hidden') && currentElement.tagName === 'UL') {
-                    currentElement.classList.remove('hidden');
-                    currentElement = currentElement.parentElement.parentElement;
-                }
-            }
+            // let currentElement = document.querySelector('#navcolumn strong');
+            // console.log(currentElement);
+            // if (currentElement) {
+            //     currentElement = currentElement.parentElement.parentElement;
+            //     while (currentElement.classList.contains('hidden') && currentElement.tagName === 'UL') {
+            //         currentElement.classList.remove('hidden');
+            //         currentElement = currentElement.parentElement.parentElement;
+            //     }
+            // }
         } else {
             navColumn.classList.add('hidden');
         }
-    });
-
-    document.querySelectorAll('#navcolumn ul').forEach(element => {
-        element.classList.add('hidden');
-        element.previousElementSibling.addEventListener('click', (e) => {
-            e.preventDefault();
-            console.log(element.classList);
-            document.querySelectorAll('#navcolumn ul').forEach(otherElement => {
-                if (otherElement !== element) {
-                    otherElement.classList.add('hidden');
-                }
-            });
-
-            let parent = element.parentElement.parentElement;
-            while (parent.classList.contains('hidden') && parent.tagName === 'UL') {
-                parent.classList.remove('hidden');
-                parent = parent.parentElement.parentElement;
-            }
-
-            if (element.classList.contains('hidden')) {
-                element.classList.remove('hidden');
-            } else {
-                element.classList.add('hidden');
-            }
-        });
     });
 
     if (window.innerWidth <= 600) {
@@ -52,5 +55,17 @@ window.addEventListener('load', () => {
         navButton.classList.add('hidden');
         navColumn.classList.remove('hidden');
     }
-    console.log(navColumn.classList)
-});
+
+    const tmp1 = navBar.children[0];
+    const tmp2 = navBar.children[1];
+    const tmp3 = navBar.children[2];
+    navBar.innerHTML = "";
+    navBar.appendChild(tmp2);
+    navBar.appendChild(document.createTextNode("|"));
+    navBar.appendChild(tmp3);
+    navBar.appendChild(document.createTextNode("|"));
+    navBar.appendChild(tmp1);
+}
+
+window.addEventListener('load', renderCollapsibleMenu);
+window.addEventListener('resize', renderCollapsibleMenu);
